908bd4da7847b7d0dd9c3ac8926ea606e4a07535
Uchijo <kgtnttki.yuta@gmail.com>
2023-09-19 16:18:07 +0900

    永続化完了

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,19 @@
 import { Layout } from "@/components/layout";
-import { SimpleModal } from "@/components/simpleModal";
 import { Todo } from "@/lib/todo";
 import { useState } from "react";
 import { Button, Col, Form, ListGroup, Row } from "react-bootstrap";
 import styles from "./index.module.css";
+import useSWR, { Fetcher } from "swr";
 
-type props = {
-  todos: Todo[];
-};
-
-export const getServerSideProps = (): { props: props } => {
-  return {
-    props: {
-      todos: [],
-    },
-  };
-};
-
-export default function Home({ todos: initialTodos }: props) {
+export default function Home() {
   const [text, setText] = useState("");
-  const [todos, setTodos] = useState<Todo[]>(initialTodos);
-  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
+
+  const {
+    data: todos,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR("/api/todos", fetcher);
 
   return (
     <>
@@ -42,15 +35,12 @@ export default function Home({ todos: initialTodos }: props) {
                 </Col>
                 <Col xs="auto">
                   <Button
-                    onClick={() => {
+                    onClick={async () => {
                       if (text.length == 0) {
-                        setIsWarningModalOpen(true);
                         return;
                       }
-                      setTodos([
-                        ...todos,
-                        { title: text, id: 100, completed: false },
-                      ]);
+                      await postTodo(text);
+                      mutate();
                       setText("");
                     }}
                     type="button"
@@ -61,39 +51,51 @@ export default function Home({ todos: initialTodos }: props) {
               </Row>
             </Form>
             <ListGroup>
-              {todos.map((todo, index) => (
-                <ListGroup.Item key={index}>
-                  <Row>
-                    <Col className="d-flex align-items-center">
-                      {todo.title}
-                    </Col>
-                    <Col xs={"auto"}>
-                      <Button
-                        variant="danger"
-                        size="sm"
-                        onClick={() => {
-                          const filteredTodo = todos.filter((_, i) => {
-                            return i != index;
-                          });
-                          setTodos(filteredTodo);
-                        }}
-                      >
-                        delete
-                      </Button>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-              ))}
+              {todos &&
+                todos.map((todo, index) => (
+                  <ListGroup.Item key={index}>
+                    <Row>
+                      <Col className="d-flex align-items-center">
+                        {todo.title}
+                      </Col>
+                      <Col xs={"auto"}>
+                        <Button variant="danger" size="sm">
+                          delete
+                        </Button>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
             </ListGroup>
-            <SimpleModal
-              isOpen={isWarningModalOpen}
-              setIsOpen={setIsWarningModalOpen}
-            >
-              <p>空白のtodoを追加することはできません</p>
-            </SimpleModal>
           </div>
         </main>
       </Layout>
     </>
   );
 }
+
+const fetcher: Fetcher<Todo[]> = async (url: string) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("network response was not ok.");
+  }
+
+  const content = (await response.json())["todos"] as Todo[];
+  return content;
+};
+
+const postTodo = async (title: string) => {
+  const response = await fetch("/api/todo/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+    }),
+  });
+  if (!response.ok) {
+    throw Error("something went wrong");
+  }
+};
